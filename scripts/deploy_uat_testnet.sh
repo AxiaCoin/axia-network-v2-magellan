c90e0ae62561238b681d03b6dfbd712fc5b27bb8
Uyauuu <jeffjing98@gmail.com>
2022-08-29 16:08:55 -0400

    update ecr for uat env

--- scripts/deploy_uat_testnet.sh
+++ scripts/deploy_uat_testnet.sh
@@ -2,8 +2,8 @@
 echo ====checkout to testnet-uat====
 git checkout testnet-uat
 echo ====docker login====
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 143953102251.dkr.ecr.us-east-1.amazonaws.com
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 667299798306.dkr.ecr.us-east-1.amazonaws.com
 echo ====build image====
-docker build -t 143953102251.dkr.ecr.us-east-1.amazonaws.com/axia-indexer-testnet-infra-magellan3ba08964-vnpvsc8ycc84:latest .
+docker build -t 667299798306.dkr.ecr.us-east-1.amazonaws.com/axia-indexer-testnet-infra-magellan3ba08964-h5eca3o7tqfq:latest .
 echo ====push image====
-docker push 143953102251.dkr.ecr.us-east-1.amazonaws.com/axia-indexer-testnet-infra-magellan3ba08964-vnpvsc8ycc84:latest
+docker push 667299798306.dkr.ecr.us-east-1.amazonaws.com/axia-indexer-testnet-infra-magellan3ba08964-h5eca3o7tqfq:latest
